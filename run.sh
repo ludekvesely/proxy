4c4636a6bb5c3efd39883658810d96f67d684993
Luděk Veselý <ludek.vesely@email.com>
2016-10-30 02:15:56 +0200

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -o nounset
+set -o errexit
+set -o pipefail
+
+# make sure it doesn't daemonize
+grep -v "daemon off" /etc/nginx/nginx.conf > /tmp/nginx.tmp && mv /tmp/nginx.tmp /etc/nginx/nginx.conf
+echo "daemon off;" >> /etc/nginx/nginx.conf
+
+# replace variables at runtime
+sed -i "s/UPSTREAM_PORT/${UPSTREAM_PORT}/g" /etc/nginx/conf.d/proxy.conf
+sed -i "s/UPSTREAM_ADDRESS/${UPSTREAM_ADDRESS}/g" /etc/nginx/conf.d/proxy.conf
+
+# start the thing
+/usr/sbin/nginx
